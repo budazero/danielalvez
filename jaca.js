fc52474f82c084dfc0d4078744f686ab58c1f403
budazero <danieah91@gmail.com>
2024-11-15 21:41:44 -0400

    Add files via upload

---
+++ jaca.js
@@ -0,0 +1,12 @@
+function aaa(){
+	var yesca=document.getElementById('nn').value;
+	var nano=document.getElementById('mm').value;
+	 if(yesca=="dani"){
+	 	if(nano=="daniel123"){
+	 		alert("correcto");
+	 		location.href="menuprincipal.html";
+	 	}
+	 	else
+	 		alert("error prueve otra vez");
+	 } 
+}
